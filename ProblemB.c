5e6de8b34e429d493d17c9e1ebe6de8597acd599
Artur.M.H.I <117451939+ARMHI@users.noreply.github.com>
2023-04-20 17:49:32 -0300

    Update ProblemB.c

--- ProblemB.c
+++ ProblemB.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-    int clock, g, e, click, pop[1], *P;
+// pi = pessoas comprometidas, gs = grupos seguros, gi= grupos inseguros
+    int clock, g, e, click, quant, gs, gi, pi, pop[1], *P;
     char  c;
     
     void testArrayP(int clock,int g){
@@ -14,7 +14,7 @@
 
     }
 
-    void FL(clock, int g){
+    void FL(int clock, int g){
 
     for ( clock = 0; clock < g; clock++){
         pop[clock]=clock+1;
@@ -40,29 +40,57 @@ int main ( ){
     
     FL(0, g);
     
-    testArrayP(0, g);
+    //testArrayP(0, g);
 
 
-    /*for (clock=0; clock < e; clock++) {
+    for (clock=0; clock < e; clock++) {
 
         scanf ("%c", &c);
 
         switch (c)
         {
         case 'c':
-            
-            break;
-        
-        case 'n':
+            int a, b;
+            scanf ("%d %d", &a, &b);
 
             break;
         
+        case 'p':
+            int c;
+            scanf("%d", &c);
+
+            break;
+
+        case 'n':
+
+            printf ("%d", quant);
+
+            break;
+        
+        case 'ns':
+
+            printf ("%d", gs);
+
+            break;
+
+        case 'ni':
+
+            printf ("%d", gi);
+
+            break;
+        
+        case 'ii':
+
+            printf ("%d", pi);
+
+            break;
+
         default:
         printf ("invalid input\n");
             break;
         }
 
-    }*/
+    }
 
 free(P);
 
