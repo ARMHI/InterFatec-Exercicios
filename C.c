690410c6b559476b8e66522c8643a9e34ffdc1de
Artur.M.H.I <117451939+ARMHI@users.noreply.github.com>
2023-05-22 11:55:16 -0300

    Update C.c

--- C.c
+++ C.c
@@ -1,81 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void entrada(char string[200], char num[400], int size, int clock){
-    fgets(string, size, stdin);
-    size = 400;
-    fgets(num, size, stdin);
 
-}
+int main (){
 
-void decode(char num[400], char alphabet[26], int alphaclock, int cleck, int sinal, int val){ 
-    if(sinal==1){
-        val = num[cleck];
-        //scanf(num[cleck], "%d", &val);
-        cleck++;
-        alphaclock = alphaclock + val;
-        if (alphaclock > 25){alphaclock=alphaclock-26;}
-    }
-    else{
-        val = num[cleck];
-        cleck++;
-        alphaclock = alphaclock - val;
-        if (alphaclock < 0){alphaclock=alphaclock+26;}
-    }
+char arrays[200] = {0}, arrayn[400] = {0}, alphabet[26]= {'a', 'b', 'c', 'd', 'e','f', 'g','h', 'i','j', 'k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-}
+int x = 1, c, alphaclock, clock, numclock = 0;
 
-void final_touches(char string[200], char *alphabet, int clock, int alphaclock){
-    string[clock]=alphabet[alphaclock];
-    printf("%s", string[clock]);
-    printf("%s", alphabet[alphaclock]);
-}
+    //entrada
+    fgets (arrays, 200, stdin);
+    fgets (arrayn, 400, stdin);
 
-void verification(char num[400], int sinal, int cleck){
-    if (num[cleck] == '-'){sinal = 0;cleck++;}
-    if (num[cleck] == '+'){sinal =1;cleck++;}
-    
-}
+    //execução
 
-void triage(char string[200], char num[400], char alphabet[26], int clock, int lengths, int alphaclock, int click, int cleck, int sinal){
+    for(clock =0; arrays [clock]!= '\0'|| clock < 200; clock++){
 
-    for (clock= 0; clock < lengths; clock++){
-
+    // discovering letter in 1st array
         alphaclock = 0;
-        while (string[clock]!=alphabet[alphaclock]&&string[clock]!='w')  {alphaclock++;}
+        while (arrays[clock] != alphabet[alphaclock]&& alphaclock < 26){alphaclock++}
+    
 
-        verification( string, sinal, cleck);
 
-       
-        if (string[clock]!='w'){decode(num, alphabet, alphaclock, cleck, sinal, 0); final_touches(string, alphabet, clock, alphaclock);}
+    // identifying if its either + or - in arrayn
+    if (arrayn[numclock] == '-' || arrayn[numclock] == '+'){
+        if (arrayn[numclock] == '-'){x = 0;}
+        if (arrayn[numclock] == '+'){x = 1;}
+        numclock++;
+    }
         
-        else{string[clock]=' ';}
-        
+    // fixing arrays   
+
+    if (x=0){ 
+        alphaclock = alphaclock - arrayn[numclock]; 
+        if (alphaclock <0){ alphaclock = 26 - alphaclock;}
+        numclock ++;
+    }
+
+    if (x=1){ 
+        alphaclock = alphaclock + arrayn[numclock]; 
+        if (alphaclock > 25){ alphaclock =  alphaclock - 26;}
+        numclock ++;
+    }
 
     }
 
-}
 
-
-
-
-
-
-
-
-int main(){
-    
-    int sinal = 1;
-    int clock, size, lengths, lengthn;
-    char string[200], num[400], alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y','z'};
-
-    entrada (string, num, 200, 0);
-
-    for (lengths=0; string[lengths]!='\0'; lengths++){}
-    lengths= lengths-1;
-    triage (string, num, alphabet, clock, lengths, 0, 0, 0, sinal);
-
-
-    free(num); return 0;
 
 }
