29594743634c0b3289bdf93e9b1ec0aef240251d
Artur.M.H.I <117451939+ARMHI@users.noreply.github.com>
2023-05-29 09:36:42 -0300

    Update B.c

--- B.c
+++ B.c
@@ -1,10 +1,23 @@
 #include <stdlib.h>
-#include <stdlib.h>
 #include <stdio.h>
 
 
 int main (){
 
+    int input1, input2, *grupos;
+
+    scanf("%d %d", input1, input2);
+
+
+    grupos = (int*) malloc (sizeof(int)*input1);
+
+
+
+    for(clock = 0; clock < input1; clock++){
+        input1[clock]= clock+1;
+    }
+
 
 }
+
 
