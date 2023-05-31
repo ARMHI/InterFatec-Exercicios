b7f9342f6febfa242ba9c6a1e576b9b6607dd087
Artur.M.H.I <117451939+ARMHI@users.noreply.github.com>
2023-05-31 11:09:22 -0300

    Update B.c

--- B.c
+++ B.c
@@ -1,19 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void group_count (int *grupos,int variant, int input1, int clock,  int counter, int carrier){
+
+    counter = 0;
+    for (clock = 0; clock<input1; clock++){
+
+            if (carrier<grupos[clock]){
+                counter++;
+                if (variant == 1){}
+                if (variant == 2){}
+
+            }
+
+
+    }
+
+}
 
 int main (){
 
-    int input1, input2, *grupos, a, b, temp, *people, clock, clock2;
+    int input1, input2, *grupos, a, b, temp, *people, clock, clock2, *covid, variant, counter;
     char cmd;
 
     scanf("%d %d", &input1, &input2);
 
     grupos = (int*) malloc (sizeof(int)*input1);
+    covid = (int*) malloc (sizeof(int)*input1);
     people = (int*) malloc (sizeof(int)*input1);
 
     for(clock = 0; clock < input1; clock++){
-        input1[clock]= clock+1;
+        grupos[clock]= clock+1;
+        people[clock]= clock+1;
+        covid[clock]= 0;
     }
 
     for (clock = 0; clock < input2; clock++){
@@ -33,8 +52,28 @@ int main (){
 
             break;
         
-        default:
+        case 'p':
+            scanf(" %d", &a);
+            covid[a-1] = 1;
             break;
+
+        case 'n':
+
+            variant = 0;
+            group_count(grupos, variant, input1, clock2, counter, 0);
+            
+            printf ("%d\n", counter);
+
+            break;
+
+        case 'n':
+
+            variant = 0;
+            group_count(grupos, variant, input1, clock2, counter, 0);
+            
+            printf ("%d\n", counter);
+            break;
+
         }
 
     }
@@ -43,6 +82,7 @@ int main (){
 
 
     free(grupos);
+    free(covid);
     free(people);
 
 }
